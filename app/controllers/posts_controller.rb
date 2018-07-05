e9d2450ce9138e62a6a0f31fa523ed99ed2797b5
Zac Willmington <zac.willmington@gmail.com>
2018-07-05 16:30:46 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,10 +12,10 @@ class PostsController < ApplicationController
 	end
 
 	def create
-		post = Post.new
-		post.title = params[:title]
-		post.description = params[:description]
-		post.save
-		redirect_to post_path
+		@post = Post.new
+		@post.title = params[:title]
+		@post.description = params[:description]
+		@post.save
+		redirect_to post_path(@post)
 	end
 end
